572fb53743971cf76b5e8aef6caa142bb8866a1e
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-08-03 17:12:44 +0200

    exported .vtp from .obj

--- franz-hsa/metopic/0760831_20210625/meshes/texture_writer.py
+++ franz-hsa/metopic/0760831_20210625/meshes/texture_writer.py
@@ -1,9 +1,40 @@
-import pyvista as pv
-# Load the .obj mesh
-mesh = pv.read('0760831_20210625.000128.obj')
-# Load the .jpg texture
-texture = pv.read_texture('0760831_20210625.000128.jpg')
-# Map the texture to the mesh
-mesh.texture_map_to_sphere(inplace=True)
-# Save the merged mesh with texture as a .vtp file
-mesh.save('merged_mesh_with_texture.vtp')
+import vtk
+
+# Set the file name of the OBJ file to be read
+obj_file_name = "0760831_20210625.000128.obj"
+obj_reader = vtk.vtkOBJReader()
+obj_reader.SetFileName(obj_file_name)
+obj_reader.Update()
+
+# Get the vtkPolyData from the reader
+polydata = obj_reader.GetOutput()
+
+# Load the texture image (assuming it's in JPEG format)
+texture_file_name = "0760831_20210625.000128.jpg"
+texture_reader = vtk.vtkJPEGReader()
+texture_reader.SetFileName(texture_file_name)
+texture_reader.Update()
+
+# Get the RGB texture data from the texture reader
+texture_data = texture_reader.GetOutput()
+print('f')
+
+# Extract the RGB values from the texture data and create a Data Array
+texture_array = vtk.vtkUnsignedCharArray()
+texture_array.SetName("Texture")
+texture_array.SetNumberOfComponents(3)  # Assuming RGB values (3 components)
+texture_array.SetNumberOfTuples(texture_data.GetNumberOfPoints())
+
+for i in range(texture_data.GetNumberOfPoints()):
+    pixel = texture_data.GetPointData().GetScalars().GetTuple(i)
+    texture_array.SetTuple3(i, pixel[0], pixel[1], pixel[2])
+
+# Add the Data Array to the vtkPolyData
+polydata.GetPointData().AddArray(texture_array)
+
+# Save the vtkPolyData with the 'Texture' Data Array as a .vtp file
+vtk_file_name = "test2.vtp"
+writer = vtk.vtkXMLPolyDataWriter()
+writer.SetFileName(vtk_file_name)
+writer.SetInputData(polydata)
+writer.Write()
