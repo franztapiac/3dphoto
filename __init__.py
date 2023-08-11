609bc1e3ba6e975408dfa9669cb6c7a5e85188a7
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-08-11 12:02:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-DATA_DIR = './data/'
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './data/')
 MODEL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './NormativePCAModel/')
 print(str(Path(MODEL_DIR).absolute()))
 CLASSIFIER_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './CraniosynostosisClassifier/')
@@ -8,9 +8,12 @@ print(str(Path(CLASSIFIER_DIR).absolute()))
 
 CRANIOFACIAL_LANDMARKING_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel.dat')
 print(str(Path(CRANIOFACIAL_LANDMARKING_MODEL_PATH).absolute()))
+
 CRANIOFACIAL_LANDMARKING_NOTEXTURE_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel-notexture.dat')
 print(str(CRANIOFACIAL_LANDMARKING_NOTEXTURE_MODEL_PATH))
+
 GLABELLA_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_glabella_new.vtp')
 print(str(GLABELLA_CRANIALBASE_LANDMARKS_PATH))
+
 EURYON_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_full_templatespace.vtp')
 print(str(EURYON_CRANIALBASE_LANDMARKS_PATH))
