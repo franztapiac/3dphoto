786f45bb06551f2f100c136f22ff5b9962e9228b
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-08-11 11:43:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import os
 DATA_DIR = '../data/'
-MODEL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './NormativePCAModel/')
-CLASSIFIER_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './CraniosynostosisClassifier/')
+MODEL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../NormativePCAModel/')
+CLASSIFIER_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../CraniosynostosisClassifier/')
 
 CRANIOFACIAL_LANDMARKING_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel.dat')
 CRANIOFACIAL_LANDMARKING_NOTEXTURE_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel-notexture.dat')
