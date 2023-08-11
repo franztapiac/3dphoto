40ac378213f369df21fd26a43f0144f5e8e41010
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-08-11 11:56:12 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 import os
+from pathlib import Path
 DATA_DIR = '../data/'
 MODEL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '/NormativePCAModel/')
-print(str(MODEL_DIR))
+print(str(Path(MODEL_DIR).absolute()))
 CLASSIFIER_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '/CraniosynostosisClassifier/')
-print(str(CLASSIFIER_DIR))
+print(str(Path(CLASSIFIER_DIR).absolute()))
 
 CRANIOFACIAL_LANDMARKING_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel.dat')
-print(str(CRANIOFACIAL_LANDMARKING_MODEL_PATH))
+print(str(Path(CRANIOFACIAL_LANDMARKING_MODEL_PATH).absolute()))
 CRANIOFACIAL_LANDMARKING_NOTEXTURE_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel-notexture.dat')
 print(str(CRANIOFACIAL_LANDMARKING_NOTEXTURE_MODEL_PATH))
 GLABELLA_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_glabella_new.vtp')
