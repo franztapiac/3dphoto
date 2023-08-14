6b7e18f6f4ebc8e0b74e1fb9085c1c494bcf86cc
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-08-14 12:24:00 +0200

    updated repo

--- franz_hsa/hsa_calculator.py
+++ franz_hsa/hsa_calculator.py
@@ -1,32 +1,9 @@
 from pathlib import Path
-from tools.DataSetGraph import WritePolyData, ReadPolyData
-from vtkmodules.vtkIOPLY import vtkPLYReader
+from tools.DataSetGraph import ReadPolyData
 from Analyze3DPhotogram import PlaceLandmarks, ComputeHSAandRiskScore
 import re
 import pandas as pd
 import os
-
-vtp_data_path = Path(
-    'C:/Users/franz/Documents/work/projects/arp/quantification-methods/hsa/3dphoto/franz-hsa/synth_data/vtp')
-ply_data_path = Path(
-    'C:/Users/franz/Documents/work/projects/arp/quantification-methods/hsa/3dphoto/franz-hsa/synth_data/ply')
-
-
-def ReadPlyfile(ply_file_path):
-    reader = vtkPLYReader()
-    reader.SetFileName(ply_file_path)
-    reader.Update()
-    poly_data = reader.GetOutput()
-    return poly_data
-
-
-def convert_ply_files_to_vtp():
-
-    for subtype_folder in ply_data_path.iterdir():
-        for mesh_ply_file_path in subtype_folder.glob('*_cp.ply'):
-            vtp_file_path = vtp_data_path / subtype_folder.name / (mesh_ply_file_path.stem + '.vtp')
-            ply_file = ReadPlyfile(str(mesh_ply_file_path))
-            WritePolyData(ply_file, str(vtp_file_path))
 
 
 def load_hsa_scores(file_path):
@@ -63,7 +40,7 @@ def export_to_excel(data_dict, output_path):
             df.to_excel(writer, sheet_name='Sheet1', startcol=i*(size+2), startrow=1, index=True)
 
 
-def calculate_hsa_scores():
+def calculate_hsa_scores(vtp_data_path):
     subtypes = ['control', 'sagittal', 'metopic']
     HSA_indeces = {subtype: {mesh_id: None for mesh_id in range(1, 101)} for subtype in subtypes}
 
@@ -87,14 +64,16 @@ def calculate_hsa_scores():
     return HSA_indeces
 
 
-def predict_and_plot(hsa_scores_path):
-    if os.path.exists(hsa_scores_path):
-        hsa_scores = load_hsa_scores(hsa_scores_path)
-    else:
-        hsa_scores = calculate_hsa_scores(hsa_scores_path)
-        export_to_excel(hsa_scores, output_path=HSA_scores_path)
-
-
 if __name__ == '__main__':
-    HSA_scores_path = Path('../franz-hsa/hsa_scores.xlsx')
-    predict_and_plot(HSA_scores_path)
+    synthetic_data_dir = Path('./synth_data/vtp_python')  # in .vtp format
+    hsa_scores_file_path = Path('hsa_scores.xlsx')
+    hsa_execution_parameters = {'age': 200,
+                                'sex': 'M',
+                                'crop': False,
+                                'crop_percentage': 0}
+
+    if os.path.exists(hsa_scores_file_path):
+        hsa_scores = load_hsa_scores(hsa_scores_file_path)
+    else:
+        hsa_scores = calculate_hsa_scores(synthetic_data_dir)
+        export_to_excel(hsa_scores, output_path=hsa_scores_file_path)
