98c59e8e872967ee6b8080916bd6582dfc04ef1a
Franz Tapia Chaca <75755705+franztapiac@users.noreply.github.com>
2023-11-08 14:29:00 +0100

    created hsav1 subdirectory in franz_hsa

---
+++ franz_hsa/hsa_v1/__init__.py
@@ -0,0 +1,12 @@
+import os
+DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './data/')
+
+MODEL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './NormativePCAModel/')
+CLASSIFIER_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), './CraniosynostosisClassifier/')
+
+CRANIOFACIAL_LANDMARKING_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel.dat')
+
+CRANIOFACIAL_LANDMARKING_NOTEXTURE_MODEL_PATH = os.path.join(DATA_DIR, 'CraniofacialLandmarkingModel-notexture.dat')
+GLABELLA_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_glabella_new.vtp')
+EURYON_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_full_templatespace.vtp')
+REDUCED_CRANIALBASE_LANDMARKS_PATH = os.path.join(DATA_DIR, 'landmarks_reduced_templatespace.vtp')
